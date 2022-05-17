a1826ad5fc66f1be7421026806ad457f9faf39ad
Mohamed Saleh <mohamedsaleh1984@hotmail.com>
2022-05-16 21:44:01 -0400

    moveZeroes

--- Move Zeroes/Move Zeroes.cpp
+++ Move Zeroes/Move Zeroes.cpp
@@ -3,13 +3,41 @@
 using namespace std;
 
 void moveZeroes(vector<int>& nums) {
+    if (nums.size() == 0 || nums.size() == 1)
+        return;
 
+    int iLeft = 0;
+    int iRight = 1;
+    int size = nums.size();
+
+    while (iRight < size) {
+
+        if (nums[iLeft] == 0 && nums[iRight] != 0) {
+            nums[iLeft] = nums[iRight];
+            nums[iRight] = 0;
+
+            iRight++;
+            iLeft++;
+        }
+        else if (nums[iLeft] == 0 && nums[iRight] == 0) {
+            iRight++;
+        }
+        else if (nums[iLeft] != 0 && nums[iRight] != 0) {
+            iRight++;
+            iLeft++;
+        }
+        else if (nums[iLeft] != 0 && nums[iRight] == 0) {
+            iRight++;
+            iLeft++;
+        }
+    }
 }
 
 
 int main()
 {
+	vector<int> nums = { 1,0 };
+	moveZeroes(nums);
 
 }
 
- 
