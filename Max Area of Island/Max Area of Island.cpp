374f6337415c749cb71b6d4febabc244b5bab925
Mohamed Saleh <mohamedsaleh1984@hotmail.com>
2022-05-28 18:57:10 -0400

    e

--- Max Area of Island/Max Area of Island.cpp
+++ Max Area of Island/Max Area of Island.cpp
@@ -33,11 +33,13 @@ public:
 		for (int i = 0; i < grid.size(); i++) {
 			for (int j = 0; j < grid[i].size(); j++) {
 				if (grid[i][j] == 1 && visited[i][j] == false) {
-					int size = computeIslandWidth(grid, i, j);
-					islandSize.push_back(size);
+					int width = 0;
+					computeIslandWidth(i, j, width);
+					islandSize.push_back(width);
 				}
 			}
 		}
+
 		if (islandSize.size()) {
 			sort(islandSize.begin(), islandSize.end());
 			return islandSize[islandSize.size() - 1];
@@ -45,70 +47,55 @@ public:
 		return 0;
 	}
 
-	int computeIslandWidth(vector<vector<int>>& grid, int row, int col) {
-		queue<vector<int>> negb;
-		int iWidth = 1;
-		negb.push({ row,col });
-		//printActions({ row,col }, "Pushed");
-			 
-		while (negb.size()) {
-			vector<int> vertex = negb.front();
-			int row = vertex[0];
-			int col = vertex[1];
-
-			//mark visited 
-			visited[row][col] = true;
-			printActions(vertex, "Marked");
-			//Remove it
-			negb.pop();
-			//printActions(vertex, "Removed from Queue");
-			//right
-
-
-			if (col + 1 < COLS && visited[row][col + 1] == false && grid[row][col + 1] == 1 ) {
-				negb.push({ row, col + 1 });
-				iWidth++;
-				visited[row][col + 1] == true;
-			//	printActions({ row, col + 1 }, "Pushed Right Neighbour");
-			}
-				
-			//Left
-			if (col - 1 >= 0 && visited[row][col - 1] == false && grid[row][col - 1] == 1) {
-
-				negb.push({ row, col - 1 });
-				iWidth++;
-				visited[row][col - 1] == true;
-			//	printActions({ row, col - 1 }, "Pushed Left Neighbour");
-			}
-				
-			//up
-			if (row - 1 >= 0 && visited[row - 1][col] == false && grid[row - 1][col] == 1) {
-				negb.push({ row - 1, col });
-				iWidth++;
-				visited[row - 1][col] == false;
-			//	printActions({ row - 1, col }, "Pushed Up Neighbour");
-			}
-				
-			//down
-			if (row + 1 < ROWS && visited[row + 1][col] == false && grid[row + 1][col] == 1) {
-				negb.push({ row + 1, col });
-				iWidth++;
-				visited[row + 1][col] = true;
-			//	printActions({ row + 1, col }, "Pushed Down Neighbour");
-			}
-			//printActions({ row,col }, "Moving to Next Vertex..");
+	vector<vector<int>>  getNeighbour(int row, int col) {
+		vector<vector<int>> negb;
+		if (col + 1 < COLS && visited[row][col + 1] == false && grid[row][col + 1] == 1) {
+			negb.push_back({ row, col + 1 });
 		}
-		return iWidth;
+
+		//Left
+		if (col - 1 >= 0 && visited[row][col - 1] == false && grid[row][col - 1] == 1) {
+			negb.push_back({ row, col - 1 });
+		}
+
+		//up
+		if (row - 1 >= 0 && visited[row - 1][col] == false && grid[row - 1][col] == 1) {
+			negb.push_back({ row - 1, col });
+		}
+
+		//down
+		if (row + 1 < ROWS && visited[row + 1][col] == false && grid[row + 1][col] == 1) {
+			negb.push_back({ row + 1, col });
+		}
+		return negb;
+	}
+
+	bool IsVisisted(vector<int> vec) {
+		return visited[vec[0]][vec[1]];
+	}
+
+	void computeIslandWidth(int row, int col, int& width) {
+		width++;
+		visited[row][col] = true;
+		vector<vector<int>> negb = getNeighbour(row, col);
+		vector<vector<int>>::iterator it;
+		for (it = negb.begin(); it != negb.end(); it++) {
+			vector<int> check = *it;
+			if (!IsVisisted(check))
+				computeIslandWidth(check[0], check[1], width);
+		}
 	}
 };
 
 int main()
 {
-	/*vector<vector<int>> grid= { {1,1,0,0,0},
+	/*vector<vector<int>> grid= { 
+								{1,1,0,0,0},
 								{1,1,0,0,0},
 								{0,0,0,1,1},
 								{0,0,0,1,1} 
-								};*/
+								};
+								*/
 	vector<vector<int>> grid = { {1,1,0,1,0},
 								 {1,0,1,1,0},
 								 {1,1,1,1,1},
