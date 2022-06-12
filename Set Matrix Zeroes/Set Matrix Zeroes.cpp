826ac538a3c1d4699d7111f849ba38a619eb4796
Mohamed Saleh <mohamedsaleh1984@hotmail.com>
2022-06-12 12:13:55 -0400

    e

--- Set Matrix Zeroes/Set Matrix Zeroes.cpp
+++ Set Matrix Zeroes/Set Matrix Zeroes.cpp
@@ -3,8 +3,36 @@
 using namespace std;
 
 //https://leetcode.com/problems/set-matrix-zeroes/
-//TODO: Do it
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        vector<vector<int>> points; 
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[0].size(); j++) {
+                if (matrix[i][j] == 0) {
+                    points.push_back({ i,j });
+                }
+            }
+        }
 
+        for (int i = 0; i < points.size(); i++) {
+            setRowZeros(matrix, points[i][0]);
+            setColumnZeros(matrix, points[i][1]);
+        }
+    }
+
+    void setRowZeros(vector<vector<int>>& matrix, int row) {
+        for (int i = 0; i < matrix[0].size(); i++) {
+            matrix[row][i] = 0;
+        }
+    }
+
+    void setColumnZeros(vector<vector<int>>& matrix, int column) {
+        for (int i = 0; i < matrix.size(); i++) {
+            matrix[i][column] = 0;
+        }
+    }
+};
 int main()
 {
 	vector< vector<int>> matrix = { {1, 1, 1},{1, 0, 1},{1, 1, 1}};
