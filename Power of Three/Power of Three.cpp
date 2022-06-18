bbee248e44ec1c1039042ab0f39b6b08b6ec8a59
Mohamed Saleh <mohamedsaleh1984@hotmail.com>
2022-06-18 13:55:01 -0400

    e

---
+++ Power of Three/Power of Three.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if (n == 0)
+            return false;
+
+        while (n % 3 == 0)
+            n = n / 3;
+        return  n == 1;
+    }
+};
+
+
+int main()
+{
+    Solution s = Solution();
+    cout << s.isPowerOfThree(27) << endl;
+    return 0;
+}
+ 
---
+++ Power of Two/Power of Two.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n == 0)
+            return false;
+
+        while (n % 3 == 0)
+            n = n / 3;
+        return  n == 1;
+    }
+};
+
+
+int main()
+{
+    Solution s = Solution();
+    cout << s.isPowerOfTwo(3) << endl;
+    return 0;
+}
