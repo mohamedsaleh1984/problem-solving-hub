716c51b89cab66e2f3d27febc6d14036f3a3c787
Mohamed Saleh <mohamedsaleh1984@hotmail.com>
2022-06-18 14:06:43 -0400

    number-of-1-bits

--- Number of 1 Bits/Number of 1 Bits.cpp
+++ Number of 1 Bits/Number of 1 Bits.cpp
@@ -2,17 +2,22 @@
 #include <bitset>
 using namespace std;
  //https://leetcode.com/problems/number-of-1-bits/
-//TODO:
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        cout << n;
+        std::bitset<32> myset(n);
+        int counter = 0;
+        for (int i = 0; i < 32; i++)
+            counter += myset[i] == 1;
+        return counter;
     }
 };
 
 int main()
 {
-  
+    Solution s = Solution();
+    cout << s.hammingWeight(5) << endl;
+
 
     return 0;
 }
