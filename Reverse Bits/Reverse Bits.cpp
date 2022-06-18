74746e77fa4a6dbeb1d110a39b27175ec1f9148b
Mohamed Saleh <mohamedsaleh1984@hotmail.com>
2022-06-18 14:03:11 -0400

    e

--- Reverse Bits/Reverse Bits.cpp
+++ Reverse Bits/Reverse Bits.cpp
@@ -2,24 +2,22 @@
 #include <bitset>
 using namespace std;
 
-uint32_t reverseBits(uint32_t n) {
-	std::bitset<32> myset(n);
-	for (int i = 0, j = 31; i < (32 / 2); i++, j--) {
-		bool tmp = myset[i];
-		myset[i] = myset[j];
-		myset[j] = tmp;
+class Solution {
+public:
+	uint32_t reverseBits(uint32_t n) {
+		std::bitset<32> myset(n);
+		for (int i = 0, j = 31; i < (32 / 2); i++, j--) {
+			bool tmp = myset[i];
+			myset[i] = myset[j];
+			myset[j] = tmp;
+		}
+		return myset.to_ulong();
 	}
-	return myset.to_ulong();
-}
+};
 
 int main()
 {
-	std::bitset<32> myset(100);
-	for (int i = 0, j = 31; i < (32 / 2); i++, j--) {
-		bool tmp = myset[i];
-		myset[i] = myset[j];
-		myset[j] = tmp;
-	}
-	cout << myset.to_ulong() << endl;
+	Solution s = Solution();
+	 
 
 }
