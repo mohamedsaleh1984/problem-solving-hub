651a48f45b8020c2ec4574aca39659ec3aa3fedc
Mohamed Saleh <mohamedsaleh1984@hotmail.com>
2022-06-27 20:53:22 -0400

    e

---
+++ Cousins in Binary Tree/Cousins in Binary Tree.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+struct TreeNode;
+struct TreeNode* newNode(int data);
+
+//https://leetcode.com/problems/cousins-in-binary-tree/
+
+class Solution {
+private: 
+public:
+	int height(TreeNode* root, int val) {
+		
+		queue<TreeNode*> Q;
+		Q.push(root);
+		
+		if (root->val == val)
+			return 1;
+		int heightToReturn = 0;
+		bool bFound = false;
+		while (!Q.empty())
+		{
+			TreeNode* topNode = Q.front();
+
+			if (topNode->left != nullptr) {
+				if (topNode->left->val == val) {
+					heightToReturn++;
+					bFound = true;
+					break;
+				}
+				else {
+					Q.push(topNode->left);
+				}
+			}
+
+
+			if (topNode->right != nullptr) {
+				if (topNode->right->val == val) {
+					heightToReturn++;
+					bFound = true;
+					break;
+				}
+				else {
+					Q.push(topNode->right);
+				}
+			}
+
+			Q.pop();
+			heightToReturn++;
+		}
+		if (bFound)
+			heightToReturn++;
+		else
+			heightToReturn = -1;
+
+		return heightToReturn;
+	}
+
+	bool isCousins(TreeNode* root, int x, int y) {
+		cout << height(root, x);
+		cout << height(root, y);
+		return false;
+	}
+};
+
+int main()
+{
+	struct TreeNode* root = newNode(1);
+	root->left = newNode(2);
+	root->right = newNode(3);
+	root->left->left = newNode(4);
+	
+	Solution s;
+
+//	cout << s.height(root, 4) << endl;
+	cout << s.isCousins(root, 2,3) << endl;
+	return 0;
+}
+
+
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+};
+
+struct TreeNode* newNode(int data)
+{
+	struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+	node->val = data;
+	node->left = NULL;
+	node->right = NULL;
+	return(node);
+}
--- Remove Duplicates from Sorted Array II/Remove Duplicates from Sorted Array II.cpp
+++ Remove Duplicates from Sorted Array II/Remove Duplicates from Sorted Array II.cpp
@@ -11,29 +11,75 @@ class Solution {
 public:
 	int removeDuplicates(vector<int>& nums) {
 		int left = 0;
-		int rigt = 1;
+		int right = 1;
 		int size = nums.size();
 
-		while (left < right) {
-			if (nums[left] != nums[right]) {
-
+		//right pointer has not reached to the end of the array
+		while (right < nums.size() - 1) {
+			//if the two adjacent items are not the same move left and right forward 
+			while(nums[left] != nums[right]) {
+				left++;
+				right++;
 			}
-		}
-		
 
+			//When the two adjacent items are equal
+			//Count the occurance of dubplicated items
+			int occuranceCounter = 0;
+			int elemsCountToPop = 0;
+			int val = nums[left];
+			int rightPtr = right;
+			//start moving the right pointer forward as long as there is duplication
+			while (val == nums[rightPtr]) {
+				++occuranceCounter;
+				rightPtr++;
+				if (rightPtr >= nums.size())
+					break;
+			}
+			//find how many items are 
+			int diff = rightPtr - left;
+			//cout << "number of occuracnes of " << val << " is " << diff << endl;
+
+			//number of occurance greater than 2 which our condition
+			if (diff > 2) {
+				//move the left pointer one step further so now the occurance are 2 in the array.
+				//Start overwrite the elements from the left side with the elements
+				//from the right side 
+				left += 2;
+				for (int i = left, j = rightPtr; j < size - 1; i++, j++) {
+					nums[i] = nums[j];
+				}
+			}
+			//number of extra items to remove from the rare of the list.
+			if (diff > 2) {
+				int pops = diff - 2;
+				while (pops > 0) {
+					nums.pop_back();
+					--pops;
+				}
+				right = left + 1;
+			}
+			else {
+				right++;
+				left++;
+			}
+			//cout << "left " << left << " , right " << right << "\n";
+		}
+		return nums.size();
 	}
 };
 int main()
 {
 	vector<int> nums = { 1,1,2 };
-	vector<int> nums2 = { 0,1,2,2,3,0,4,2 };
+	vector<int> nums2 = { 0,0,0,1,1,1,1,2,2,3,3 };
 	vector<int> nums3 = { 2,2,3 };
 
 	Solution s = Solution();
 
 	cout << s.removeDuplicates(nums) << endl;
 	cout << s.removeDuplicates(nums2) << endl;
-	cout << s.removeDuplicates(nums2) << endl;
+	cout << s.removeDuplicates(nums3) << endl;
+	
+
 
 	return 0;
 }
