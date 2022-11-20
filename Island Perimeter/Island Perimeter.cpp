78768ec5b5917f06ba2a395b6347da72043e3006
Mohamed Saleh <mohamedsaleh1984@hotmail.com>
2022-11-19 22:17:16 -0500

    int islandPerimeter(vector<vector<int>>& grid) {

--- Island Perimeter/Island Perimeter.cpp
+++ Island Perimeter/Island Perimeter.cpp
@@ -5,55 +5,18 @@
 #include <iomanip>
 using namespace std;
 //https://leetcode.com/problems/island-perimeter/
-//TODO: Write the algo
 class Solution {
 private:
-	vector<vector<bool>> visited;
-	vector<vector<int>> grid;
-	int COLS;
-	int ROWS;
 public:
-	//void printActions(vector<int> point, string action) {
-	//	cout << action << " [" << point[0] << "," << point[1] << "]\n";
-	//}
-	//
-	//void viewGrid(vector<vector<int>> grid) {
-	//	int rows = grid.size();
-	//	int cols = grid[0].size();
-	//	cout << "[";
-	//	for (int i = 0; i < rows; i++)
-	//	{
-	//		cout << "[";
-	//		for (int j = 0; j < cols; j++)
-	//		{	
-	//			cout << setw(2) << grid[i][j] << " ";
-	//		}
-	//		cout << "]";
-	//		cout << endl;
-	//	}
-	//	cout << "]";
-	//}
-
-	//void init(vector<vector<int>> grid) {
-	//	this->grid = grid;
-	//	for (int i = 0; i < grid.size(); i++)
-	//	{
-	//		vector<bool> row(grid[i].size());
-	//		visited.push_back(row);
-	//	}
-	//	ROWS = grid.size();
-	//	COLS = grid[0].size();
-	//}
-
 	int islandPerimeter(vector<vector<int>>& grid) {
 		int perimeter = 0;
 		int rows = grid.size();
 		int cols = grid[0].size();
 		for (int iRow = 0; iRow < rows; iRow++)
 		{
-			int defaultPerimeter = 4;
 			for (int iCol = 0; iCol < cols; iCol++)
 			{
+				int defaultPerimeter = 4;
 				if (grid[iRow][iCol] == 1)
 				{
 					//left 
@@ -63,7 +26,7 @@ public:
 							defaultPerimeter--;
 					}
 					//right
-					if (iCol < grid[0].size() - 1)
+					if (iCol < cols - 1)
 					{
 						if (grid[iRow][iCol + 1] == 1)
 							defaultPerimeter--;
@@ -76,56 +39,25 @@ public:
 					}
 
 					//down
-					if (iRow < grid[0].size() - 1)
+					if (iRow < rows - 1)
 					{
 						if (grid[iRow + 1][iCol] == 1)
 							defaultPerimeter--;
 					}
+
+					perimeter += defaultPerimeter;
 				}
 
 			}
 		}
 		return perimeter;
 	}
-
-	//vector<vector<int>>  getNeighbour(int row, int col) {
-	//	vector<vector<int>> negb;
-	//	if (col + 1 < COLS && visited[row][col + 1] == false && grid[row][col + 1] == 1) {
-	//		negb.push_back({ row, col + 1 });
-	//	}
-
-	//	//Left
-	//	if (col - 1 >= 0 && visited[row][col - 1] == false && grid[row][col - 1] == 1) {
-	//		negb.push_back({ row, col - 1 });
-	//	}
-
-	//	//up
-	//	if (row - 1 >= 0 && visited[row - 1][col] == false && grid[row - 1][col] == 1) {
-	//		negb.push_back({ row - 1, col });
-	//	}
-
-	//	//down
-	//	if (row + 1 < ROWS && visited[row + 1][col] == false && grid[row + 1][col] == 1) {
-	//		negb.push_back({ row + 1, col });
-	//	}
-	//	return negb;
-	//}
-
-	//bool IsVisisted(vector<int> vec) {
-	//	return visited[vec[0]][vec[1]];
-	//}
-
- //   int islandPerimeter(vector<vector<int>>& grid) {
-	//	init(grid);
-	//	vector<int> islandSize = {};
-
- //   }
 };
 int main()
 {
 	Solution s;
-
-	vector<vector<int>> grid = { {0, 1, 0, 0}, { 1, 1, 1, 0 }, { 0, 1, 0, 0 }, { 1, 1, 0, 0 }};
+	vector<vector<int>> grid1 = { {0, 1, 0, 0}, { 1, 1, 1, 0 }, { 0, 1, 0, 0 }, { 1, 1, 0, 0 }};
+	vector<vector<int>> grid = { {1,0}};
 	cout << s.islandPerimeter(grid);
 
     return 0;
