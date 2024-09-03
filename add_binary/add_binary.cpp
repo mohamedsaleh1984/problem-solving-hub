bff0f7862fc30bf0e3553970d24c244f03014d82
Mohamed <mohamedsaleh1984@hotmail.com>
2024-09-03 11:10:32 -0400

    Programming/CPP/problem-solving-hub/add_binary

--- add_binary/add_binary.cpp
+++ add_binary/add_binary.cpp
@@ -1,57 +1,57 @@
- 
+
 #include <iostream>
 #include <string>
 using namespace std;
 
-//https://leetcode.com/problems/add-binary/submissions/
-class Solution {
+// https://leetcode.com/problems/add-binary/submissions/
+class Solution
+{
 public:
-    string addBinary(string a, string b) {
-        //Padding Zeros
-        if (a.size() > b.size())
-        {
-            int diff = a.size() - b.size();
-            while (diff--)
-                b.insert(0, "0");
-        }
-        else if (b.size() > a.size()) {
-            int diff = b.size() - a.size();
-            while (diff--)
-                a.insert(0, "0");
-        }
+    string addBinary(string a, string b)
+    {
+        // Padding Zeros
+        int diff = (a.size() > b.size()) ? a.size() - b.size() : b.size() - a.size();
+
+        while (diff--)
+            b.insert(0, "0");
 
         string result = "";
         char carry = '0';
-        //Calc and Push from front...
-        for (int i = a.size() - 1; i >= 0; i--) {
+        // Calc and Push from front...
+        for (int i = a.size() - 1; i >= 0; i--)
+        {
             char tmpA = a[i];
             char tmpB = b[i];
 
             if (tmpA == tmpB && tmpA == '1')
             {
-                if (carry == '0') {
+                if (carry == '0')
+                {
                     result.insert(0, "0");
                     carry = '1';
                 }
-                else {
+                else
+                {
                     result.insert(0, "1");
                     carry = '1';
                 }
-
             }
             else if (tmpA == tmpB && tmpA == '0')
             {
-                if (carry == '1') {
+                if (carry == '1')
+                {
                     result.insert(0, "1");
                     carry = '0';
                 }
-                else {
+                else
+                {
                     result.insert(0, "0");
                 }
             }
             else if (tmpA != tmpB)
             {
-                if (carry == '1') {
+                if (carry == '1')
+                {
                     result.insert(0, "0");
                     carry = '1';
                 }
