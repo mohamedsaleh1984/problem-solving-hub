7a7919d85b733406c09ce1bdb39573c711053b92
Mohamed <mohamedsaleh1984@hotmail.com>
2024-10-01 13:43:06 -0400

    contains_near_duplicate

--- contains_duplicate_ii/contains_duplicate_ii.cpp
+++ contains_duplicate_ii/contains_duplicate_ii.cpp
@@ -10,10 +10,15 @@ https://leetcode.com/problems/contains-duplicate-ii/
 bool containsNearbyDuplicate(vector<int> &nums, int k)
 {
     unordered_map<int, int> mp;
+    //go throuigh each item
     for (int i = 0; i < nums.size(); i++)
     {
+        //if current element was visited or exists in the map
+        //and the diffent of the current index and stored elem fullfill the condition.
         if (mp.count(nums[i]) && abs(mp[nums[i]] - i) <= k)
             return true;
+
+        //store the Value and the index
         mp[nums[i]] = i;
     }
     return false;
