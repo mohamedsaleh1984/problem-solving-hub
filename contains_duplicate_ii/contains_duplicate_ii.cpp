95086af64e2d6d7099216d88d67f1e283fe55dd7
Mohamed <mohamedsaleh1984@hotmail.com>
2024-10-01 13:40:42 -0400

    contains_near_duplicate

--- contains_duplicate_ii/contains_duplicate_ii.cpp
+++ contains_duplicate_ii/contains_duplicate_ii.cpp
@@ -7,10 +7,12 @@ using namespace std;
 https://leetcode.com/problems/contains-duplicate-ii/
 133ms Beats31.60%
 */
-bool containsNearbyDuplicate(vector<int>& nums, int k) {
-    unordered_map<int, int>mp;
-    for (int i = 0; i < nums.size(); i++) {
-        if (mp.count(nums[i]) && abs(mp[nums[i]] - i) <= k) 
+bool containsNearbyDuplicate(vector<int> &nums, int k)
+{
+    unordered_map<int, int> mp;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (mp.count(nums[i]) && abs(mp[nums[i]] - i) <= k)
             return true;
         mp[nums[i]] = i;
     }
@@ -18,8 +20,7 @@ bool containsNearbyDuplicate(vector<int>& nums, int k) {
 }
 int main()
 {
-    vector<int> nums = { 1, 2, 3, 1 };
+    vector<int> nums = {1, 2, 3, 1};
     int k = 3;
     cout << containsNearbyDuplicate(nums, k) << endl;
 }
- 
