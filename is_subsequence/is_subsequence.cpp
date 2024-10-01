2fa803ef1ef988afc7bc26421578335faacc2c46
Mohamed <mohamedsaleh1984@hotmail.com>
2024-10-01 09:02:00 -0400

    maximum-product-subarray

--- is_subsequence/is_subsequence.cpp
+++ is_subsequence/is_subsequence.cpp
@@ -2,17 +2,46 @@
 #include <vector>
 using namespace std;
 
-//https://leetcode.com/problems/is-subsequence/
-//TODO:Write it
-class Solution {
+// https://leetcode.com/problems/is-subsequence/
+
+class Solution
+{
 public:
-    bool isSubsequence(string s, string t) {
-        return true;
+    bool isSubsequence(string s, string t)
+    {
+        int grid[s.size()][t.size()];
+        for (int x = 0; x < s.size() * t.size(); x++)
+            *((int *)grid + x) = 0;
+
+        int prev = 0;
+        int j = 0;
+        int i = 0;
+
+        while (s[0] != t[j])
+            j++;
+
+        for (int i = 0; i < s.size(); i++)
+        {
+            for (; j < t.size(); j++)
+            {
+                if (s[i] == t[j])
+                {
+                    grid[i][j] = prev + 1;
+                    prev = grid[i][j];
+                }
+                else
+                {
+                    grid[i][j] = 0;
+                }
+            }
+        }
+
+        return s.size() == grid[s.size() - 1][t.size() - 1];
     }
 };
 int main()
 {
-    string s, t;
+    string s = "aaaaaa", t = "bbaaaa";
     Solution sol = Solution();
-    cout << sol.isSubsequence(s, t);
+    cout << sol.isSubsequence(s, t) << endl;
 }
---
+++ maximum-product-subarray/maximum-product-subarray.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+using namespace std;
+
+/*
+    https://leetcode.com/problems/maximum-product-subarray/submissions/
+    3ms Beats 85.56%
+ */
+
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int result = nums[0];
+        int prevMax = nums[0];
+        int prevMin = nums[0];
+
+        for(int i = 1 ; i < nums.size() ; i++){
+
+            int currMax = max(nums[i] , max(prevMax*nums[i], prevMin*nums[i]));
+            int currMin = min(nums[i] , min(prevMax*nums[i], prevMin*nums[i]));
+
+            prevMin = currMin;
+            prevMax = currMax;
+
+            result = max(prevMax, result);
+
+        }
+
+        return result;
+    }
+};
+
+int main(void){
+    return 0;
+}
