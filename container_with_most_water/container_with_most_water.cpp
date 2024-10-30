28b7ef0f7b41f040772c08a7af9b4da764f1604d
Mohamed Saleh <mohamedsaleh1984@hotmail.com>
2024-10-30 11:39:04 -0400

    Update container_with_most_water.cpp

--- container_with_most_water/container_with_most_water.cpp
+++ container_with_most_water/container_with_most_water.cpp
@@ -3,21 +3,19 @@
 using namespace std;
 //https://leetcode.com/problems/container-with-most-water
 int maxArea(vector<int>& height) {
-    int left = 0, right = height.size() - 1;
-    int iMax = 0;
-
-    while (left < right) {
-        int area = (right - left) * min(height[left], height[right]);
-        iMax = max(iMax, area);
-        if (height[left] < height[right])
-            left++;
-        else if (height[left] > height[right])
-            right--;
-        else
-            right--;
-    }
-
-    return iMax;
+    int left = 0, right = height.size()-1;
+        int iMax = 0;
+    
+        while(left < right){
+            int area = (right-left) * min(height[left],height[right]);
+            iMax = max(iMax,area);
+            if(height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+        
+        return iMax;
 }
 
 int main()
