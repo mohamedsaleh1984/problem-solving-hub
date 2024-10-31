287a1a309a1e0a0eb993e4d5f56528cb0f658b3a
Mohamed <mohamedsaleh1984@hotmail.com>
2024-10-31 09:02:13 -0400

    is-subsequent

--- is_subsequence/is_subsequence.cpp
+++ is_subsequence/is_subsequence.cpp
@@ -9,34 +9,20 @@ class Solution
 public:
     bool isSubsequence(string s, string t)
     {
-        int grid[s.size()][t.size()];
-        for (int x = 0; x < s.size() * t.size(); x++)
-            *((int *)grid + x) = 0;
+        int p1 = 0;
+        int p2 = 0;
 
-        int prev = 0;
-        int j = 0;
-        int i = 0;
-
-        while (s[0] != t[j])
-            j++;
-
-        for (int i = 0; i < s.size(); i++)
-        {
-            for (; j < t.size(); j++)
-            {
-                if (s[i] == t[j])
-                {
-                    grid[i][j] = prev + 1;
-                    prev = grid[i][j];
-                }
-                else
-                {
-                    grid[i][j] = 0;
-                }
+        while(p2 < t.size()){
+            if(s[p1] == t[p2]){
+                p1++;
+                p2++;
+            }
+            else{
+                p2++;
             }
         }
 
-        return s.size() == grid[s.size() - 1][t.size() - 1];
+        return p1 == s.size() && p2 == t.size();
     }
 };
 int main()
