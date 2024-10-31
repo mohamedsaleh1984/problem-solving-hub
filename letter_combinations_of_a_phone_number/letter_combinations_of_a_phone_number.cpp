dbbf4f7a47df4dcf94ec9c33b48d88b6a464fea0
Mohamed <mohamedsaleh1984@hotmail.com>
2024-10-31 08:42:01 -0400

    letter combination

--- letter_combinations_of_a_phone_number/letter_combinations_of_a_phone_number.cpp
+++ letter_combinations_of_a_phone_number/letter_combinations_of_a_phone_number.cpp
@@ -2,12 +2,17 @@
 #include <vector>
 #include <map>
 using namespace std;
+
 //https://leetcode.com/problems/letter-combinations-of-a-phone-number/
-//TODO: Fix it
+//0 ms Beats 100.00%
 class Solution {
 public:
 
-    Solution() {
+    vector<string> letterCombinations(string digits) {
+        vector<string> resultSet;
+        if(digits.length()==0)
+            return resultSet;
+
         mapper.insert(pair<char, string>{'2', "abc"});
         mapper.insert(pair<char, string>{'3', "def"});
         mapper.insert(pair<char, string>{'4', "ghi"});
@@ -16,33 +21,26 @@ public:
         mapper.insert(pair<char, string>{'7', "pqrs"});
         mapper.insert(pair<char, string>{'8', "tuv"});
         mapper.insert(pair<char, string>{'9', "wxyz"});
-    }
 
-    vector<string> letterCombinations(string digits) {
-        vector<string> resultSet;
         string entry;
-        string keysToHit = mapper[0];
-        Solve(digits,entry,resultSet);
+        int index = 0;
+        Solve(digits,entry,resultSet,index);
 
         return resultSet;
-
     }
    
-    void Solve(string digits,string& entry, vector<string>& resultSet) {
-      
-
-        string stroks = mapper[digits[0]];
-
-        for (int i = 0; i < stroks.length(); i++) {
-            
-
-
-
+    void Solve(string digits,string& entry, vector<string>& resultSet, int index) {
+        if(entry.size() == digits.size()){
+            resultSet.push_back(entry);
+            return;
         }
-        
-        
-
-       
+ 
+        string value  = mapper[digits[index]];
+        for(int i = 0 ; i < value.length() ; i++){
+           string newEntry =  entry;
+           newEntry.push_back(value[i]);
+           Solve(digits,newEntry,resultSet,index+1);
+        }
     }
 
 private:
@@ -63,35 +61,11 @@ int main()
     Solution s;
 
     string digits = "249";
-    cout << s.computeCombinations(digits);
-    
+       
     vector<string> vec = s.letterCombinations(digits);
     for (auto a : vec)
         cout << a << " ";
 
     return 0;
 }
-
-/*
- //234
-    int computeCombinations(string digits) {
-        int comp = 1;
-        for (int i = 0; i < digits.length(); i++) {
-            string codePerDigit = mapper[digits[i]];
-            comp *= codePerDigit.length();
-        }
-        return comp;
-    }
-    vector<string> letterCombinations(string digits) {
-        vector<string> result;
-        string codes = mapper[digits[0]];
-        for (int i = 0; i < 3; i++)
-        {
-            string comb;
-            comb.push_back(codes[i]);
-            result.push_back(comb);
-        }
-
-        return result;
-    }
-*/
+ 
