8d59e300974b0300969d2bb6ac84d3fd8fca08e9
Mohamed <mohamedsaleh1984@hotmail.com>
2024-11-06 23:03:03 -0500

    find-the-index-of-the-first-occurrence-in-a-string

---
+++ find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <iomanip>
+using namespace std;
+
+// https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+// 2ms Beats 13.01%
+class Solution {
+public:
+    bool isMatching(string s, string e, int start){
+        for(int i = 0, j = start ; i < e.length() ; i++,j++){
+            if(s[j] != e[i])
+                return false;
+        }
+        return true;
+    }
+
+    int strStr(string haystack, string needle) {
+        int index = -1;
+        int lenOfNeedle = needle.size();
+        int lenOfHay = haystack.size();
+
+        for(int i = 0 ; i < lenOfHay ; i++){
+            int rem = lenOfHay - i;
+            if(haystack[i] == needle[0] && rem>=lenOfNeedle && isMatching(haystack,needle,i)){
+                return i;
+            }
+        }
+        return index;    
+    
+    }
+};
+
+
+int main(void){ 
+    //haystack = "sadbutsad", needle = "sad";
+    string haystack = "leetcode", needle = "leeto";
+    Solution s;
+    cout << s.strStr(haystack,needle) << endl;
+
+    return 0;   
+}
