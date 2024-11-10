ca6d0184cb0817b9dbfbf0cad6a6a4eb9dd02919
Mohamed <mohamedsaleh1984@hotmail.com>
2024-11-10 15:59:49 -0500

    counting-bits

---
+++ integer-to-roman/integer-to-roman.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+// https://leetcode.com/problems/integer-to-roman/?envType=problem-list-v2&envId=math
+class Solution
+{
+public:
+    string repeatChars(char c, int n)
+    {
+        string s = "";
+        while (n--)
+            s += c;
+        return s;
+    }
+
+    string intToRoman(int num)
+    {
+        string strOutput = "";
+        int c = 0;
+        while (num > 0)
+        {
+            if (num >= 1000)
+            {
+                c = num / 1000;
+                num = num - (1000 * c);
+                strOutput += repeatChars('M', c);
+            }
+            else if (num >= 500)
+            {
+                c = num / 500;
+                num = num - (500 * c);
+                strOutput += repeatChars('D', c);
+            }
+            else if (num >= 100)
+            {
+                c = num / 100;
+                num = num - (100 * c);
+                strOutput += repeatChars('C', c);
+            }
+            else if (num >= 50)
+            {
+                c = num / 50;
+                num = num - (50 * c);
+                strOutput += repeatChars('L', c);
+            }
+            else if (num >= 10)
+            {
+                c = num / 10;
+                num = num - (10 * c);
+                strOutput += repeatChars('X', c);
+            }
+            else if (num >= 5)
+            {
+                c = num / 5;
+                num = num - (5 * c);
+                strOutput += repeatChars('V', c);
+            }
+            else
+            {
+                strOutput += repeatChars('I', num);
+                num = 0;
+            }
+        }
+
+        return strOutput;
+    }
+};
+
+struct TestCase
+{
+    int num;
+    string expectedOutput;
+
+    TestCase(
+        int num,
+        string expectedOutput)
+    {
+        this->num = num;
+        this->expectedOutput = expectedOutput;
+    }
+};
+
+void runTest(int num, string expectedOutcome)
+{
+    Solution s;
+    string outcome = s.intToRoman(num);
+
+    if (expectedOutcome == outcome)
+        cout << "✅ Successful" << endl;
+    else
+    {
+        cout << "❌ Failed" << endl;
+        cout << "Expected output " << expectedOutcome << ", your output is " << outcome << endl;
+    }
+}
+
+void runTestCases(vector<TestCase> ex)
+{
+    for (int i = 0; i < ex.size(); i++)
+    {
+        runTest(ex.at(i).num,ex.at(i).expectedOutput);
+    }
+}
+
+int main(void)
+{
+    vector<TestCase> vec = {{3749,"MMMDCCXLIX"}
+        //,{58,"LVIII"},{1994,"MCMXCIV"}
+    };
+    runTestCases(vec);
+
+    return 0;
+}
--- merge_sorted_array/merge_sorted_array.cpp
+++ merge_sorted_array/merge_sorted_array.cpp
@@ -2,6 +2,26 @@
 #include <vector>
 using namespace std;
 
+struct mergeSortedArrayTestCase
+{
+    vector<int> nums1;
+    int m;
+    vector<int> nums2;
+    int n;
+    vector<int> expectedOutput;
+
+    mergeSortedArrayTestCase(vector<int> nums1, int m, vector<int> nums2, int n, vector<int> expectedOutput)
+    {
+        this->nums1 = nums1;
+        this->nums2 = nums2;
+        this->m = m;
+        this->n = n;
+        this->expectedOutput = expectedOutput;
+    }
+};
+
+void printOut(vector<int> res);
+
 // https://leetcode.com/problems/merge-sorted-array/
 //
 class Solution
@@ -9,8 +29,7 @@ class Solution
 public:
     void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
     {
-        int p1 = 0;
-        int p2 = 0;
+        int p1 = 0, p2 = 0;
         int zeroIndex = m;
 
         while (p2 < nums2.size())
@@ -36,9 +55,13 @@ public:
                     zeroIndex++;
 
                     // place the element from righ to left side
-                    nums1[p1] = nums2[p2++];
+                    nums1[p1] = nums2[p2];
+                    // move right side forward
+                    p2++;
+                    // move left side forward
+                    p1++;
                 }
-                else
+                else if (nums1[p1] == 0 && nums2[p2])
                 {
                     nums1[zeroIndex] = nums2[p2];
                     p2++;
@@ -50,30 +73,53 @@ public:
     }
 };
 
-int main()
+void runTest(vector<int> &nums1, int m,
+             vector<int> nums2, int n,
+             vector<int> expectedOutcome)
 {
-    vector<int> nums1 = {1, 2, 3, 0, 0, 0};
-    int m = 3;
-    vector<int> nums2 = {2, 5, 6};
-    int n = 3;
-
-    // vector<int> nums1 = {1}; int m = 1;
-    // vector<int> nums2 = {}; int n = 0;
-
-    // vector<int> nums1 = {0}; int m = 0;
-    // vector<int> nums2 = {1}; int n = 1;
-
-    // vector<int> nums1 = {4,5,6,0,0,0}; int m = 3;
-    // vector<int> nums2 = {1,2,3}; int n = 3;
-
-    // vector<int> nums1 = {-1,0,0,3,3,3,0,0,0}; int m = 6;
-    // vector<int> nums2 = {1,2,2}; int n = 3;
-
     Solution s;
     s.merge(nums1, m, nums2, n);
 
-    for (int i = 0; i < nums1.size(); i++)
+    if (expectedOutcome == nums1)
+        cout << "✅ Successful" << endl;
+    else
     {
-        cout << nums1.at(i) << " ";
+        cout << "❌ Failed" << endl;
+        cout << "\tYour output is ";
+        printOut(nums1);
+        printOut(nums2);
+        cout << " and expected output is ";
+        printOut(expectedOutcome);
+        cout << endl;
     }
+}
+
+void runTestCases(vector<mergeSortedArrayTestCase> ex)
+{
+    for (int i = 0; i < ex.size(); i++)
+    {
+        runTest(ex.at(i).nums1, ex.at(i).m, ex.at(i).nums2, ex.at(i).n, ex.at(i).expectedOutput);
+    }
+}
+
+int main()
+{
+    vector<mergeSortedArrayTestCase> vec = {{{1, 2, 3, 0, 0, 0}, 3, {2, 5, 6}, 3, {1, 2, 2, 3, 5, 6}},
+                            {{1}, 1, {}, 0, {1}},
+                            {{0}, 0, {1}, 1, {1}},
+                            {{4, 5, 6, 0, 0, 0}, 3, {1, 2, 3}, 3, {1, 2, 3, 4, 5, 6}}};
+
+    runTestCases(vec);
+}
+
+void printOut(vector<int> res)
+{
+    cout << "[";
+    for (int i = 0; i < res.size(); i++)
+    {
+        cout << res[i];
+        if (i + 1 != res.size())
+            cout << ",";
+    }
+    cout << "]";
 }
