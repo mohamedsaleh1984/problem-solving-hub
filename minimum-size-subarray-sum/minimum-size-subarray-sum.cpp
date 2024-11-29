94eb3baa83590002b12c6404cfe0e8bf86b5a381
Mohamed <mohamedsaleh1984@hotmail.com>
2024-11-29 11:18:24 -0500

    minimum size subarray sum

---
+++ minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <cstdlib>
+using namespace std;
+
+// https://leetcode.com/problems/minimum-size-subarray-sum/
+// 0ms Beats 100.00%
+
+class Solution
+{
+public:
+    int minSubArrayLen(int target, vector<int> &nums)
+    {
+        int res = INT_MAX;
+        int left = 0, right = 0;
+        int len = nums.size();
+        int currentSum = 0;
+
+        while (right < len)
+        {
+            currentSum += nums[right];
+            while (currentSum >= target)
+            {
+                res = min(res, right - left + 1);
+
+                currentSum -= nums[left];
+                left++;
+            }
+            right++;
+        }
+        return res == INT_MAX ? 0 : res;
+    }
+ 
+};
+
+int main(int argc, char **argv)
+{
+    vector<int> nums = {1, 1, 1, 1, 1, 1, 1, 1};
+    Solution s;
+    cout << s.minSubArrayLen(7, nums) << endl;
+
+    return 0;
+}
