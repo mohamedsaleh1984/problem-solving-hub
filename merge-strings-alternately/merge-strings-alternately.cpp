738646571440869c7258651c3cc3f29177a82211
Mohamed <mohamedsaleh1984@hotmail.com>
2024-12-01 15:10:00 -0500

    d

--- merge-strings-alternately/merge-strings-alternately.cpp
+++ merge-strings-alternately/merge-strings-alternately.cpp
@@ -2,8 +2,35 @@
 #include <vector>
 using namespace std;
 
+// 12/01/2024
 // https://leetcode.com/problems/merge-strings-alternately
-// 
+// 3ms Beats 44.38%
+
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int l = min(word1.length(), word2.length());
+        int i = 0;
+        string sout;
+
+        while(i<l){
+            sout+=word1.at(i);
+            sout+=word2.at(i);
+            i++;
+        }
+
+        if(word1.length() != l){
+            sout+= word1.substr(l,word1.length());
+        }
+
+        if(word2.length() != l){
+            sout+= word2.substr(l,word2.length());
+        }
+        
+
+        return sout;
+    }
+};
 int main()
 {
 
---
+++ reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -0,0 +1,46 @@
+
+
+
+// https://leetcode.com/problems/reverse-vowels-of-a-string/submissions/1467727825/?envType=study-plan-v2&envId=leetcode-75
+// 8msBeats 9.63%
+// 12/01/2024
+
+class Solution {
+public:
+    bool isVowel(char c) {
+        if (c == 'a' || c == 'A' || c == 'e' || c == 'E' || c == 'i' ||
+            c == 'I' || c == 'o' || c == 'O' || c == 'u' || c == 'U')
+            return true;
+        return false;
+    }
+    string reverseVowels(string s) {
+        string strOut;
+        unordered_map<int, char> postions;
+        int pos = 0;
+        int len = s.length();
+
+        // first pass to get all vowels
+        for (int i = 0; i < len; i++) {
+            if (isVowel(s[i])) {
+                postions.insert({pos, s[i]});
+                pos++;
+            }
+        }
+
+        pos--;
+
+        for (int i = 0; i < len; i++) {
+            if (isVowel(s[i])) {
+                strOut += postions[pos];
+                pos--;
+            } else {
+                strOut += s[i];
+            }
+        }
+
+        return strOut;
+    }
+};
+
+
+
