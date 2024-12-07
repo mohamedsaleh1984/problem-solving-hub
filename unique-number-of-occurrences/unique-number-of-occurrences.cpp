10aa46e6f08f408fc727ae4b28fb161ae6387b0c
Mohamed <mohamedsaleh1984@hotmail.com>
2024-12-07 11:44:47 -0500

    occur

---
+++ unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <set>
+#include <map>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        map<int,int> occur;
+        
+        for(int i = 0 ;  i< arr.size() ; i++){
+            if(occur.count(arr[i])){
+                occur[arr[i]]++;
+            }
+            else
+            {
+                occur[arr[i]]=1;
+            }
+        }
+        
+        int msize = occur.size();
+        set<int> s;
+        map<int,int>::iterator it=occur.begin();
+        while(it != occur.end())
+        {
+            s.insert(it->second);
+            it++;
+        }
+            
+        int ssize = s.size();
+
+        return  ssize == msize;
+    }
+};
+
+int main(int argc, char **argv)
+{
+    vector<int> vec = {1,2,2,1,1,3};
+    Solution s;
+    cout << s.uniqueOccurrences(vec) << endl;
+
+    return 0;
+}
