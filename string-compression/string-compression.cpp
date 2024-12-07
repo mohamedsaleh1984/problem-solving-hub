6a6fc811de512acc016c5cfd13e670aa15e2f6af
Mohamed <mohamedsaleh1984@hotmail.com>
2024-12-07 11:06:53 -0500

    medium

---
+++ string-compression/string-compression.cpp
@@ -0,0 +1,94 @@
+#include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
+
+template <class T>
+void view(vector<T> vec){
+    for(auto x: vec)
+        cout << x;
+    cout << endl;
+}
+// https://leetcode.com/problems/string-compression/
+// 0 ms Beats 100.00%
+class Solution
+{
+public:
+    int compress(vector<char> &chars)
+    {
+        view(chars);
+        // single char return 1
+        if (chars.size() == 1)
+            return 1;
+
+        unsigned int index = 0;
+        unsigned int count = 1;
+        unsigned int len = chars.size();
+        vector<char> vec;
+
+        while (index < len - 1)
+        {
+            if (chars[index] == chars[index + 1])
+            {
+                count++;
+            }
+            else
+            {
+                // pushing char a
+                vec.push_back(chars[index]);
+
+                string str = to_string(count);
+                // check count
+                if (count > 1)
+                {
+                    for (size_t i = 0; i < str.length(); i++)
+                    {
+                        vec.push_back(str[i]);
+                    }
+                }
+                count = 1;
+            }
+            index++;
+        }
+
+        //
+        // pushing char a
+        vec.push_back(chars[index]);
+
+        string str = to_string(count);
+        // check count
+        if (count > 1)
+        {
+            for (size_t i = 0; i < str.length(); i++)
+            {
+                vec.push_back(str[i]);
+            }
+        }
+
+        view(vec);
+        //
+        for (size_t i = 0; i < vec.size(); i++) {
+            chars[i] = vec[i];
+        }
+
+        cout << "************************************" << endl;
+        return vec.size();
+    }
+};
+
+int main(int argc, char **argv)
+{
+    Solution s;
+
+    vector<char> v = {'a', 'a', 'b', 'b', 'c', 'c', 'c'};
+    vector<char> v2 = {'a', 'a'};
+    vector<char> v3 = {'a', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'};
+    vector<char> v4 = {'a'};
+
+    cout << s.compress(v) << endl;
+    cout << s.compress(v2) << endl;
+    cout << s.compress(v3) << endl;
+    cout << s.compress(v4) << endl;
+
+    return 0;
+}
---
+++ the-kth-factor-of-n/the-kth-factor-of-n.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+#include <set>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+// https://leetcode.com/problems/the-kth-factor-of-n
+// 0ms Beats 100.00%
+
+class Solution {
+public:
+    int kthFactor(int n, int k) {
+        vector<int> fac;
+        int counter = 0;
+        for(int i = 1 ;  i<= n ; i++){
+            if(n%i == 0){
+                fac.push_back(i);
+                counter++;
+                if(counter == k)
+                    break;
+            }
+        }
+
+        if(fac.size() < k)
+            return -1;
+        return fac[k-1];
+    }
+};
+
+
+
+
+int main(int argc,char** argv){
+    Solution s;
+    cout << s.kthFactor(4,4) << endl;
+    return 0;
+}
