a14953100276f932d05cb76c0fbde17c21dfe423
Mohamed <mohamedsaleh1984@hotmail.com>
2024-12-15 15:38:06 -0500

    Blind 75

--- linked-list-cycle/linked-list-cycle.cpp
+++ linked-list-cycle/linked-list-cycle.cpp
@@ -5,18 +5,40 @@
  
 class Solution {
 public:
-    bool hasCycle(ListNode *head) {
+    // O(1) using fast and slow pointer
+    bool hasCycle(ListNode* head) {
+        if (head == nullptr)
+            return false;
+        if (head->next == nullptr) {
+            return false;
+        }
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast)
+                return true;
+        }
+
+        return false;
+    }
+    // O(N) using sets
+    bool hasCycle2(ListNode* head) {
         if (head == nullptr)
             return false;
         set<ListNode*> nodes;
         ListNode* tmp = head;
-        while(tmp){
-            if(nodes.count(tmp)){
+        while (tmp) {
+            if (nodes.count(tmp)) {
                 return true;
-            }else{
+            } else {
                 nodes.insert(tmp);
             }
-            
+
             tmp = tmp->next;
         }
 
---
+++ maximum-average-subarray-i/maximum-average-subarray-i.cpp
@@ -0,0 +1,67 @@
+#include "../common.h"
+
+// https://leetcode.com/problems/maximum-average-subarray-i/?envType=study-plan-v2&envId=leetcode-75
+// 12/15/2024
+// 0 ms Beats 100.00%
+class Solution
+{
+private:
+    double maxAvg = INT_MIN;
+    double curSum = 0;
+    double curAvg = 0;
+    int k;
+
+public:
+    double findMaxAverage(vector<int> &nums, int k)
+    {
+        this->k = k;
+        int len = nums.size();
+
+        if (nums.size() == 0)
+            return 0;
+
+        if (nums.size() == 1)
+            return nums[0];
+
+        int left = 0;
+        int right = k;
+        // compute the sum for first Window
+        for (int i = left; i < right; i++)
+            curSum += nums[i];
+        
+        setMaxAvg();
+
+        while (right < len)
+        {
+            // remove first number
+            curSum -= nums[left];
+            left++;
+            curSum += nums[right];
+
+            setMaxAvg();
+
+            right++;
+        }
+
+        return maxAvg;
+    }
+    void setMaxAvg()
+    {
+        // compute the avg for first Window
+        curAvg = curSum / k;
+
+        if (curAvg > maxAvg)
+            maxAvg = curAvg;
+    }
+};
+
+int main(int argc, char **argv)
+{
+    Solution s;
+    vector<int> vec = {1, 12, -5, -6, 50, 3};
+    vector<int> vec2 = {0, 1, 1, 3, 3};
+
+    cout << s.findMaxAverage(vec2, 4) << endl;
+
+    return 0;
+}
