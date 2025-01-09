06db088d2a4bf6889a74f1112acae2abd9981b0f
Mohamed Saleh <mohamedsaleh1984@hotmail.com>
2025-01-09 11:35:59 -0500

    Update leaf-similar-trees.cpp

--- leaf-similar-trees/leaf-similar-trees.cpp
+++ leaf-similar-trees/leaf-similar-trees.cpp
@@ -1,22 +1,9 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-// https://leetcode.com/problems/leaf-similar-trees/submissions/1473328293/?envType=study-plan-v2&envId=leetcode-75
+#include "../common.h"
+
+// https://leetcode.com/problems/leaf-similar-trees
 // 0ms Beats 100.00%
 // 08/12/2024
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
- * };
- */
 class Solution {
 public:
     void leafs(TreeNode* root, vector<int>& ns){
