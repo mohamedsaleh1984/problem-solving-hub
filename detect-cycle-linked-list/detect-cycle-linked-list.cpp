fca626a46ad21f6f7941cdee3cd449ce36186a4d
Mohamed Saleh <mohamedsaleh1984@hotmail.com>
2025-01-09 11:12:13 -0500

    Update detect-cycle-linked-list.cpp

--- detect-cycle-linked-list/detect-cycle-linked-list.cpp
+++ detect-cycle-linked-list/detect-cycle-linked-list.cpp
@@ -1,9 +1,4 @@
-// DetectCycleLinkedList.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
-#include <string>
-using namespace std;
+#include "../common.h"
 
 class SinglyLinkedListNode {
 public:
