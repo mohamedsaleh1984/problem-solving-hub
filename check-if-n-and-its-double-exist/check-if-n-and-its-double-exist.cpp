ecde3e200aee0e8b6b476c75fb57e5b4ec7a8b51
Mohamed Saleh <mohamedsaleh1984@hotmail.com>
2025-01-09 11:01:51 -0500

    Update check-if-n-and-its-double-exist.cpp

--- check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
+++ check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -1,7 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <map>
-using namespace std;
+#include "../common.h"
 
 //https://leetcode.com/problems/check-if-n-and-its-double-exist/?envType=daily-question&envId=2024-12-01
 //
