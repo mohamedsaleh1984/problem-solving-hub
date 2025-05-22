e66e10907834c61b5464a1c4f261a1f9d26fcdc3
Mohamed Saleh <mohamedsaleh1984@hotmail.com>
2025-05-22 11:01:45 -0400

    alternative solution

--- count_good_nodes_in_binary_tree/count_good_nodes_in_binary_tree.cpp
+++ count_good_nodes_in_binary_tree/count_good_nodes_in_binary_tree.cpp
@@ -5,9 +5,21 @@
 Runtime: 161 ms, faster than 65.61% of C++ online submissions for Count Good Nodes in Binary Tree.
 */
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-	int goodNodes(TreeNode* root) {
+/*
+      int goodNodes(TreeNode* root) {
 		int currentMax = root->val;
 		int mxLeft = goodNodesHelper(root->left, currentMax);
 		int mxRight = goodNodesHelper(root->right, currentMax);
@@ -27,8 +39,44 @@ public:
 			return  goodNodesHelper(root->left, currentMax) + goodNodesHelper(root->right, currentMax);
 		}
 	}
-};
+   */
+   /*
+   	int goodNodes(TreeNode* root){
+		int count = 0;
+		goodNodesHelper(root,root->val,count);
+		return count;
+	}
 
+	int goodNodesHelper(TreeNode* root, int prevMax,int& count){
+		if(root == NULL){
+			return 0;
+		}
+
+		if(root->val >= prevMax){
+            count++;
+            prevMax = root->val;
+        }
+		
+		return  goodNodesHelper(root->left, prevMax,count) + goodNodesHelper(root->right, prevMax,count);
+	}
+    */
+  int goodNodes(TreeNode* root){
+		if(!root)
+            return 0;
+	    return goodNodes(root,root->val);
+		
+	}
+    int goodNodes(TreeNode* root, int maxSoFar){
+		if(!root){
+			return 0;
+		}
+
+        int addme = root->val >= maxSoFar;
+        int l = goodNodes(root->left, max(maxSoFar, root->val));
+        int r = goodNodes(root->right, max(maxSoFar, root->val));  
+        return addme + l + r;
+	}
+};
 
 
 
